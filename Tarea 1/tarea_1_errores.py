89541f5369bbbfad92954120faeedc226b95c7ba
DanQue678 <141382762+DanQue678@users.noreply.github.com>
2023-08-04 22:05:50 -0600

    Archivo con 3 errores

---
+++ Tarea 1/tarea_1_errores.py
@@ -0,0 +1,16 @@
+import math
+import random
+
+
+def PrimeOrNot(p):
+    result = True
+    i = 2
+    while i <= math.sqrt(p):
+        if p % i == 0:
+            result = False
+            break
+        i += 1
+    return p, result;
+
+# print(PrimeOrNot(random.randint(2,10000000)))
+
